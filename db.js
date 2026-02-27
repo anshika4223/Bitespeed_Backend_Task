61b9a6c5b25f25276d6be4c99de73ac352f63a25
Anshika Puri <anshikapuri270603@gmail.com>
2026-02-27 23:07:34 +0530

    Refactor database connection setup in db.js
    Updated database connection configuration to use individual parameters from environment variables.

--- db.js
+++ db.js
@@ -1,11 +1,24 @@
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: process.env.NODE_ENV === "production"
+//     ? { rejectUnauthorized: false }
+//     : false,
+// });
+
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false, // REQUIRED on Render
+  },
 });
 
 module.exports = pool;
